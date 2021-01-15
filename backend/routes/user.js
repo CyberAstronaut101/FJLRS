677ff2f9cfdcea68194d4375f354c74bda2857b8
Elliot <ekid113@yahoo.com>
2021-01-14 21:04:09 -0600

    hotfix bcrypt -> bcrypt-nodejs package update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt-nodejs");
 const jwt = require("jsonwebtoken");
 
 const checkAuth = require("../middleware/check-auth");
